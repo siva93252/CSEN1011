64fcfe034df63d6aaf9fe61d3c7959e211e06104
siva93252 <93367874+siva93252@users.noreply.github.com>
2021-11-02 14:23:37 +0530

    Add files via upload

---
+++ siva00.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? a;
+
+   raptor_prompt_variable_zzyz ="enter a number";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> a;
+   if (a % 2==0)
+   {
+      cout << "the number is even" << endl;   }
+   else
+   {
+      cout << "the number is odd " << endl;   }
+
+   return 0;
+}
